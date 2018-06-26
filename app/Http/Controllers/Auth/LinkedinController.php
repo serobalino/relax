50b9306801b3f0ee1ffc7593b313596ca906bbbd
SebastiaN <odm_sd@hotmail.com>
2018-06-26 16:08:12 -0500

    importar clase

--- app/Http/Controllers/Auth/LinkedinController.php
+++ app/Http/Controllers/Auth/LinkedinController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Laravel\Socialite\Facades\Socialite;
 
 class LinkedinController extends Controller
 {
